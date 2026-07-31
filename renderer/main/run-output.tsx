117d1b343106fbfca9bc4ede61bab761fc9317e1
Chris <chris.desteuben@gmail.com>
2026-07-30 18:55:28 -0700

    This panel's background should always be opaque. I can see the script text in the background. Fix this.
    [commit-id:9dde3a86bd36b47eae1f99f4165f87351c0c4091]

--- renderer/main/run-output.tsx
+++ renderer/main/run-output.tsx
@@ -14,7 +14,7 @@ export function RunOutput({ info }: { info: RunInfo }) {
   }
 
   return (
-    <div className="flex h-56 shrink-0 flex-col border-t border-separator">
+    <div className="bg-background flex h-56 shrink-0 flex-col border-t border-separator">
       <div className="flex items-center gap-2 border-b border-separator px-4 py-2">
         <Text variant="small-strong">Output</Text>
         {info.running ? (
